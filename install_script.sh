1ec4330e87b9deadf2a320b25b9bb5c69ff23135
akshayjn93 <akshayjn93@gmail.com>
2019-07-14 14:36:30 +0530

    script changes

--- install_script.sh
+++ install_script.sh
@@ -17,5 +17,5 @@ chown -R 1000:1000 /var/jenkins_home/
 docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins
 
 # show endpoint
-echo 'Jenkins installed'
-echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
+echo 'JENkins INstalled'
+echo 'You should Now be able to access JENKINS at: http://'$(curl -s ifconfig.co)':8080'
